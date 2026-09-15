22b69fe31524f0ea6028e8e496f6178fd9bcdbbb
Haard Patel <haardp9@gmail.com>
2026-09-15 01:06:39 -0600

    BOX

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -193,6 +193,14 @@ function About() {
 
         {/* FOCUS TAGS */}
         <div className="about-focus">
+        tags: [
+      "MUSIC",
+      "FITNESS",
+      "MATHEMATICS",
+      "READING",
+      "ADVENTURE",
+      "SPORTS",
+    ],
           <span>MUSIC</span>
           <span>FITNESS</span>
           <span>MATHEMATICS</span>
